fddfc34b3eb250ddd1e2104c962c028718daba1e
fwcd <fwcdmail@gmail.com>
2018-08-22 20:23:07 +0200

    Ensure a consistent look and feel across platforms

--- src/main/java/com/fwcd/breeze/BreezeMain.java
+++ src/main/java/com/fwcd/breeze/BreezeMain.java
@@ -1,9 +1,16 @@
 package com.fwcd.breeze;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import com.fwcd.breeze.core.BreezeApp;
 
 public class BreezeMain {
 	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+		} catch (ClassNotFoundException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException e) {}
+		
 		new BreezeApp("Breeze", 480, 640, true);
 	}
 }
